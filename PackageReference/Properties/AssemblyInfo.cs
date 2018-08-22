e6b18e55d9e06c224469540d43f93d5f723d42fb
Leonid Tsarev <leo@bastilia.ru>
2018-08-22 19:49:41 +0300

    initial project creation

--- PackageReference/Properties/AssemblyInfo.cs
+++ PackageReference/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PackagesConfig")]
+[assembly: AssemblyTitle("PackageReference")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PackagesConfig")]
+[assembly: AssemblyProduct("PackageReference")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4c040d3c-eaee-4560-bf87-00c62ea32682")]
+[assembly: Guid("39d39e8e-66f5-4be7-8109-b5f5582ae6b2")]
 
 // Version information for an assembly consists of the following four values:
 //
